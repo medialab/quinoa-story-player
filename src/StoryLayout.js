9f509c37092b5c6f13c9d5564fb4153534e22057
Robin de Mourat <robin.demourat@gmail.com>
2017-04-03 12:12:09 +0200

    improve scrolling behavior

--- src/StoryLayout.js
+++ src/StoryLayout.js
@@ -13,6 +13,7 @@ class PresentationLayout extends Component {
     this.scrollToCover = this.scrollToCover.bind(this);
     this.handleSpringUpdate = this.handleSpringUpdate.bind(this);
     this.scrollTop = this.scrollTop.bind(this);
+    this.onScrollUpdate = this.onScrollUpdate.bind(this);
 
     this.state = {
       inCover: true
@@ -23,36 +24,40 @@ class PresentationLayout extends Component {
     this.springSystem = new SpringSystem();
     this.spring = this.springSystem.createSpring();
     this.spring.addListener({onSpringUpdate: this.handleSpringUpdate});
-
-    // const isFirefox = typeof InstallTrigger !== 'undefined';
-    // if (this.globalScrollbar)  {
-    //   this.globalScrollbar.refs.view.addEventListener(isFirefox ? 'wheel' : 'mousewheel', (e) => {
-    //     if (!this.state.inCover) {
-    //       e.preventDefault();
-    //       e.stopPropagation();
-    //     }
-    //   }, false);
-    // }
   }
   scrollToContents () {
-    const scrollHeight = this.globalScrollbar.getScrollHeight();
-    this.scrollTop(scrollHeight / 2);
+    this.scrollTop(this.header.offsetHeight);
     this.setState({
       inCover: false
     });
   }
   scrollToCover () {
-    this.scrollTop(-1000);
+    this.scrollTop(0);
     this.setState({
       inCover: true
     });
+  }
+
+  onScrollUpdate (evt) {
+    const scrollTop = evt.scrollTop;
+    const headerHeight = this.header.offsetHeight;
+    if (scrollTop < headerHeight && !this.state.inCover) {
+      this.setState({
+        inCover: true
+      });
+    }
+ else if (scrollTop > headerHeight && this.state.inCover) {
+      this.setState({
+        inCover: false
+      });
+    }
   }
 
   scrollTop(top) {
         const scrollbars = this.globalScrollbar;
         const scrollTop = scrollbars.getScrollTop();
         const scrollHeight = scrollbars.getScrollHeight();
-        const val = MathUtil.mapValueInRange(top, 0, scrollHeight, scrollHeight * 0.2, scrollHeight * 0.8);
+        const val = MathUtil.mapValueInRange(top, 0, scrollHeight, 0, scrollHeight);
         this.spring.setCurrentValue(scrollTop).setAtRest();
         this.spring.setEndValue(val);
     }
@@ -68,18 +73,26 @@ class PresentationLayout extends Component {
         content
       }
     } = this.props;
+    const {
+      inCover
+    } = this.state;
     const bindGlobalScrollbarRef = scrollbar => {
       this.globalScrollbar = scrollbar;
+    };
+    const bindHeaderRef = header => {
+      this.header = header;
     };
     return (
       <section className="wrapper">
         <Scrollbars
           ref={bindGlobalScrollbarRef}
           autoHide
+          onUpdate={this.onScrollUpdate}
           universal>
           <header
             onClick={this.scrollToContents}
             className="header"
+            ref={bindHeaderRef}
             style={{
               backgroundImage: metadata.coverImage ? 'url(' + metadata.coverImage + ')' : undefined
             }}>
@@ -105,7 +118,11 @@ class PresentationLayout extends Component {
               <Renderer raw={content} />
             </section>
 
-            <nav className="nav">
+            <nav
+              className="nav"
+              style={{
+                position: inCover ? 'relative' : 'fixed'
+              }}>
               <h2 onClick={this.scrollToCover}>{metadata.title || 'Quinoa story'}</h2>
             </nav>
           </section>
