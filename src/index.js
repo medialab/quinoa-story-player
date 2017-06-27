4e2edaacf7f2c3fda3eb5eff6609c5e540219a6b
Robin de Mourat <robin.demourat@gmail.com>
2017-06-27 09:07:46 +0200

    provide more info for templates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Player from './Player';
 
+// informations about the templates that the module is able to provide
 const templateInfo = [
   require('./templates/garlic/info')
 ];
-
-
 export const templates = templateInfo;
 
+/**
+ * Default template is a react component
+**/
 export default Player;
